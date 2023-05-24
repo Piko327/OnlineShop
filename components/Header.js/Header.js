bf9b06e1a9db8fa84b9d53d12dafb3bd60c03a75
Piko327 <pikopikop372@gmail.com>
2023-05-24 20:41:41 +0300

    remove heroicons add react-icons

--- components/Header.js/Header.js
+++ components/Header.js/Header.js
@@ -3,13 +3,14 @@ import Navbar from "./Navbar";
 import Image from "next/image";
 
 const Header = () => {
+
   return (
     <section className="flex justify-between">
       <span className=" font-bold p-4 self-center bg-slate-900 text-slate-100"
        >T-Shop</span>
       <Navbar />
 
-      <div>{/*cart icon*/}</div>
+<BsCartDashFill/>
 
       {true ? (
         <div>
