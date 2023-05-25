3506f5e13c0651e509bccb78305d5c072d162767
Piko327 <pikopikop372@gmail.com>
2023-05-25 19:57:17 +0300

    Update Header.js

--- components/Header.js/Header.js
+++ components/Header.js/Header.js
@@ -4,20 +4,23 @@ import Navbar from "./Navbar";
 import { BsCartDashFill } from "react-icons/bs";
 import SingIn from "../SingIn/SingIn";
 
-
 const Header = () => {
   return (
-    <section className="flex justify-between">
+    <section className=" flex justify-between">
+    {/*Logo */}
       <span className=" font-bold p-4 self-center bg-slate-900 text-slate-100">
         T-Shop
       </span>
+      
       <Navbar />
 
-      <div className="flex p-2 relative">
-        <BsCartDashFill className="text-2xl" />
-        <p className=" bg-gray-800 text-slate-50 absolute top-1 right-12">0</p>
-       
-       <SingIn/>
+      <div className="flex">
+        <div className="flex p-2 relative">
+          <BsCartDashFill className="text-2xl " />
+          <p className=" bg-gray-800 text-slate-50 absolute top-1 right-1">0</p>
+        </div>
+        
+        <SingIn />
       </div>
     </section>
   );
