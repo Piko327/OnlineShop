de821abffc7ceb32fbe61e4f0462b03a45198957
Piko327 <pikopikop372@gmail.com>
2023-05-26 12:58:08 +0300

    responsive nav is ready

--- components/Header.js/Header.js
+++ components/Header.js/Header.js
@@ -7,32 +7,34 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import SingIn from "../SingIn/SingIn";
 
 const Header = () => {
-  const [active, setActive] = useState(true);
+  const [activeNav, setActiveNav] = useState(false);
 
-  const onclick=()=>
-  { setActive(!active)
-  }
+  const activeNavHandler = () => {
+    setActiveNav(!activeNav);
+  };
+
   return (
     <section className="px-1 py-2 flex bg-slate-50 justify-between items-center border-solid border-b-2 border-sky-500">
       {/*Logo */}
       <span className="text-2xl italic font-bold">T-Shop</span>
 
-      <Navbar props={active}/>
+      <Navbar props={{ activeNav, activeNavHandler }} />
       <div className="flex">
         <SingIn />
         <div className=" lg:flex p-2 relative items-center">
-          <BsCartDashFill className="text-2xl " />
-          <p className=" bg-gray-800 p-0.5 text-slate-50 absolute top-4 right-1">
+          <BsCartDashFill className="text-2xl mt-6 lg:mt-1" size={30} />
+          <p className=" bg-gray-800 p-0.5 text-slate-50 absolute top-6 right-1">
             0
           </p>
         </div>
-        <div  className="lg:hidden flex " onClick={onclick}>
-          {" "}
-          {active ? (
+        <div className="lg:hidden flex " onClick={activeNavHandler}>
+   
+          {activeNav ? (
             <AiOutlineClose className="self-center ml-4 w-8 h-8 lg:hidden" />
           ) : (
             <AiOutlineMenu className="self-center ml-4 w-8 h-8 lg:hidden" />
           )}
+
         </div>
       </div>
     </section>
--- components/Header.js/Navbar.js
+++ components/Header.js/Navbar.js
@@ -1,15 +1,20 @@
-'use client'
+"use client";
+
 import Link from "next/link";
-import React from "react";
-import uniqid from 'uniqid';
-const Navbar = ({active}) => {
-  
+import uniqid from "uniqid";
+const Navbar = ({ props }) => {
   return (
     <>
-     {
+      {
         //Mobile
       }
-      <nav className='w-2/3 absolute  flex-col lg:hidden  text-center bg-slate-50 text-gray-700 left-0 top-28  shadow-md border-solid border-b-2 border-r-2 border-gray-400'>
+      <nav
+        className={
+          props.activeNav
+            ? "w-2/3 absolute  flex-col lg:hidden  text-center bg-slate-50 text-gray-700 left-0 top-28  shadow-md border-solid border-b-2 border-r-2 border-gray-400"
+            : "hidden"
+        }
+      >
         <ul className=" flex-col text-2xl py-12  h-96">
           {" "}
           {[
@@ -18,14 +23,16 @@ const Navbar = ({active}) => {
             ["Shop", "/products"],
             ["Card", "/payment"],
           ].map(([title, url]) => (
-            <li key={uniqid()} className="m-2 p-2 border-solid border-b-2 border-gray-200 hover:text-gray-900 ease-in duration-150">
+            <li
+              onClick={() => props.activeNavHandler()}
+              key={uniqid()}
+              className="m-2 p-2 border-solid border-b-2 border-gray-200 hover:text-gray-900 ease-in duration-150"
+            >
               <Link href={url}>{title}</Link>
             </li>
           ))}{" "}
         </ul>
-      </nav>
-     {" "}
-
+      </nav>{" "}
       <nav className=" hidden lg:block">
         <ul className="flex text-2xl  justify-evenly items-center">
           {" "}
@@ -35,7 +42,10 @@ const Navbar = ({active}) => {
             ["Shop", "/products"],
             ["Card", "/payment"],
           ].map(([title, url]) => (
-            <li key={uniqid()} className="m-2 p-2 hover:text-gray-900 ease-in duration-150">
+            <li
+              key={uniqid()}
+              className="m-2 p-2 hover:text-gray-900 ease-in duration-150"
+            >
               <Link href={url}>{title}</Link>
             </li>
           ))}{" "}
