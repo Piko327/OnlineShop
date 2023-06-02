05bea0d727b07320ffd577a81655e0407a1176d0
Piko327 <pikopikop372@gmail.com>
2023-06-02 16:51:05 +0300

    start with add future products

--- app/page.js
+++ app/page.js
@@ -1,13 +1,27 @@
 import Baner from "@/components/Baner/Baner";
+import FutureProducts from "@/components/Products/FutureProducts";
+import Products from "@/components/Products/Products";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <selection >
-      <Baner />
+    <selection>
+      <div className="mb-[60vh]">
+     
+        <Baner />
+        <h1 className="text-3xl   sm:text-5xl p-2  text-gray-300 border-2 font-bold relative  text-center z-30 top-40 mx-6 lg:text-6xl">
+          ORDER NOW FOR THE LATEST TRENDS
+        </h1>
+        <Link
+          href="/Shop"
+          className=" text-3xl md:text-4xl px-3 rounded-xl  lg:text-5xl  border-1  bg-rose-950  italic p-2  text-gray-300 border-2 relative  z-30 top-60  left-[30vw]  sm:left-[45vw] sm:top-[30vh]  ">
+          Shop now
+        </Link>
+      </div>
 
-      <h1 className='text-3xl   sm:text-5xl p-2  text-gray-300 border-2 font-bold relative  text-center z-30 top-40 mx-6 lg:text-6xl' >ORDER NOW FOR THE LATEST TRENDS</h1>
-      <Link href="/Shop" className=' text-3xl md:text-4xl px-3 rounded-xl  lg:text-5xl  border-1  bg-rose-950  italic p-2  text-gray-300 border-2 relative  z-30 top-60  left-[30vw]  sm:left-[40vw] sm:top-[40vh] lg:left-[70vw] '>Shop now</Link>
+<FutureProducts/>
+
+    
     </selection>
   );
 }
---
+++ components/Products/FutureProducts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FutureProducts = () => {
+  return (
+    <div>FutureProducts
+
+
+        <></>
+    </div>
+  )
+}
+
+export default FutureProducts
