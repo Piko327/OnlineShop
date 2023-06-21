2278517d4ee8edfc49fe560e533b88eb7c4cfa48
Piko327 <pikopikop372@gmail.com>
2023-06-21 16:54:24 +0300

    drobdown problem fixed

--- app/Shop/page.js
+++ app/Shop/page.js
@@ -7,19 +7,18 @@ import { React, useState } from "react";
 const Shop = () => {
   const [category, setCategory] = useState("All");
   return (
-    <>   <Baner src="/asset/backgroundShop.jpg" />
-    <div className="bg-gray-200  flex flex-col min-h-[70vh] mt-96  ">
-  
-      <h2 className="absolute top-40 right-10  z-20 text-center backdrop-blur-xl text-rose-900 border-red-100 border-solid border-2 p-5 m-3  text-2xl  font-bold italic">
-        Discover the Thrills of Online Shopping:
-         Your Gateway to Unforgettable
-        Finds!
-      </h2>
+    <>
+      {" "}
+      <Baner src="/asset/backgroundShop.jpg" />
+      <div className="bg-gray-200  flex flex-col min-h-[70vh] mt-96  ">
+      <h2 className="absolute top-40 opacity-2 z-20 text-center backdrop-blur-sm text-rose-200  p-5 m-3  text-2xl  font-bold italic">
+          Discover the Thrills of Online Shopping: Your Gateway to Unforgettable
+          Finds!
+        </h2>
+        <Dropdown setCatagory={setCategory} />
 
-      <Dropdown setCatagory={setCategory} />
-
-      <Products category={category} />
-    </div>
+        <Products category={category} />
+      </div>
     </>
   );
 };
--- components/DropDown/Dropdown.js
+++ components/DropDown/Dropdown.js
@@ -13,10 +13,10 @@ export default function Dropdown({ setCatagory }) {
 
   return (<div>
       <Menu>
-        <Menu.Button className="bg-rose-950 self-start  text-gray-100  text-right p-2 m-2 ml-10 md:ml-20">
+        <Menu.Button className="bg-rose-950 rounded-sm self-start  text-gray-100  text-right p-2 m-2 ml-10 md:ml-20">
           Catagories
         </Menu.Button>
-        <Menu.Items className="bg-gray-100 p-4 flex flex-col  shadow-lg absolute   top-52 left-12 gap-5">
+        <Menu.Items className="bg-gray-100 p-4 flex flex-col  shadow-lg absolute    left-12 gap-5">
           {catagoryTitles.map((title) => (
             /* Use the `active` state to conditionally style the active item. */
             <Menu.Item key={title} as={Fragment}>
